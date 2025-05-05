a36f59515fa8c74f37f085febb9e6334165cad28
HK <sharkhk@example.com>
2025-05-06 00:16:40 +0400

    Deploy: VulneraX full working final version

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, send_file
+app = Flask(__name__)
+
+@app.route("/")
+def homepage():
+    return render_template("index.html")
+
+@app.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
+
+@app.route("/report")
+def report():
+    return render_template("report.html")
+
+@app.route("/admin")
+def admin():
+    return render_template("admin_panel.html")
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=5000)
