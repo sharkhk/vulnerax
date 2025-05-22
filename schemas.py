7b5c602439aa76f0205dec45cb5010094b357311
sharkhk <hk80300@hotmail.com>
2025-05-22 17:13:22 +0400

    Update schemas.py

--- schemas.py
+++ schemas.py
@@ -1,9 +1,5 @@
 from marshmallow import Schema, fields
 
-class CVEQuerySchema(Schema):
-    days = fields.Int(missing=1)
-    limit = fields.Int(missing=50)
-
 class CVESchema(Schema):
     id = fields.Str()
     description = fields.Str()
