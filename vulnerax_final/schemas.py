9e1282df8a7e7ad768c3813cb33e8a070e504b7a
sharkhk <hk80300@hotmail.com>
2025-05-22 17:49:12 +0400

    Delete vulnerax_final directory

--- vulnerax_final/schemas.py
+++
@@ -1,8 +0,0 @@
-from marshmallow import Schema, fields
-
-class CVESchema(Schema):
-    id = fields.Str()
-    description = fields.Str()
-    publishedDate = fields.Str()
-    lastModifiedDate = fields.Str()
-    cvssScore = fields.Float(allow_none=True)
