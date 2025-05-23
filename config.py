95c502cfa2b4108a6e62d90a6c2e002e3160b72f
sharkhk <hk80300@hotmail.com>
2025-05-23 17:39:23 +0400

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class Config:
     DEBUG = ENV == "development"
     TESTING = False
     NVD_API_URL = "https://services.nvd.nist.gov/rest/json/cves/1.0"
-    NVD_API_KEY = os.getenv("NVD_API_KEY")
+   NVD_API_KEY = "7fbc28b6-88f1-4c43-adbd-a6428ed31135"
     CACHE_TYPE = "SimpleCache"
     CACHE_DEFAULT_TIMEOUT = 300  # seconds
     REPORTS_DIR = os.getenv("REPORTS_DIR", "./reports")
