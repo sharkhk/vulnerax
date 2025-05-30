1562de6a51876c833738913d8d729ec85026c236
sharkhk <hk80300@hotmail.com>
2025-05-31 02:53:35 +0400

    Delete celery_app.py

--- celery_app.py
+++
@@ -1,7 +0,0 @@
-from celery import Celery
-from config import Config
-
-def make_celery(app_name=__name__):
-    return Celery(app_name, broker=Config.CELERY_BROKER_URL, backend=Config.CELERY_RESULT_BACKEND)
-
-celery = make_celery()
