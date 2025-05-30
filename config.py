6b768bed5a4dde50afcfc8bb5c04bf2388149504
sharkhk <hk80300@hotmail.com>
2025-05-31 03:11:32 +0400

    Update config.py

--- config.py
+++ config.py
@@ -5,9 +5,12 @@ class Config:
     ENV = os.getenv("FLASK_ENV", "production")
     DEBUG = ENV == "development"
     TESTING = False
+    # NVD API and CIRCL feed URLs
+    NVD_API_URL = "https://services.nvd.nist.gov/rest/json/cves/1.0"
     CIRCL_API_URL = "https://cve.circl.lu/api"
+    NVD_API_KEY = os.getenv("NVD_API_KEY", "7fbc28b6-88f1-4c43-adbd-a6428ed31135")
     CACHE_TYPE = "SimpleCache"
-    CACHE_DEFAULT_TIMEOUT = 300
+    CACHE_DEFAULT_TIMEOUT = 300  # seconds
     REPORTS_DIR = os.getenv("REPORTS_DIR", "./reports")
 
 class DevelopmentConfig(Config):
